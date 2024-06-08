d4c35131a65731348535c1bbd3957f5b98ca2be6
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-06-08 17:29:58 +0200

    client navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import Link from "next/link"
 
 function Navbar() {
+    const pathname = usePathname()
+
     return (
         <nav>
             <div>
-                <Link href="/">Inicio</Link>
-                <Link href="/articulos">Artículos</Link>
-                <Link href="/proveedores">Proveedores</Link>
+                <Link href="/"
+                    className={pathname == '/' ? 'item-selected' : ''} >
+                    Inicio
+                </Link>
+                <Link href="/articulos"
+                    className={pathname.startsWith('/articulos') ? 'item-selected' : ''} >
+                    Artículos
+                </Link>
+                <Link href="/proveedores"
+                    className={pathname.startsWith('/proveedores') ? 'item-selected' : ''} >
+                    Proveedores
+                </Link>
             </div>
             <div>
-                <Link href="/acerca">Acerca de ...</Link>
+                <Link href="/acerca"
+                    className={pathname.startsWith('/acerca') ? 'item-selected' : ''} >
+                    Acerca de ...
+                </Link>
             </div>
         </nav>
     )
